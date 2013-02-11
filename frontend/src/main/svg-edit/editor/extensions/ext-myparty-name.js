2274334f7366129b3738197d2291c0931bc1d284
nicolas.moreaud@gmail.com <nicolas.moreaud@gmail.com@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-02-11 21:51:43 +0000

    TicketDesigner : added name interactor

---
+++ frontend/src/main/svg-edit/editor/extensions/ext-myparty-name.js
@@ -0,0 +1,80 @@
+/*
+ * ext-myparty-name.js
+ *
+ * Licensed under the GPL License
+ *
+ * Copyright(c) 2013 Nicolas Moreaud, Réda Lyazidi
+ *
+ */
+
+svgEditor.addExtension("mypartyName", function() {
+	var canv = svgEditor.canvas;
+	//var svgroot = canv.getRootElem();
+	var mode_id = 'myparty_name';
+    var newText = null;
+    var started = false;
+
+	return {
+		svgicons: "extensions/ext-myparty-name.xml",
+		buttons: [{
+			id: "tool_myparty_name",
+			type: "mode",
+			//position: 10,
+			title: "Add a person name to this party ticket",
+			events: {
+				"click": function() {
+					canv.setMode(mode_id);
+				}
+			}
+		}],
+
+		mouseDown: function(opts) {
+			var mode = canv.getMode();
+			if(mode !== mode_id) return;
+			
+			//var e = opts.event;
+			var x = opts.start_x;
+			var y = opts.start_y;
+			//var cur_style = canv.getStyle();
+            var cur_text = canv.getAllProperties().text;
+            var cur_shape= canv.getAllProperties().shape;
+            
+            newText = canv.addSvgElementFromJson({
+                "element": "text",
+                "curStyles": true,
+                "attr": {
+                    "x": x,
+                    "y": y,
+                    "id": canv.getNextId(),
+                    "fill": cur_text.fill,
+                    "stroke-width": cur_text.stroke_width, // can be accessed by canv.getStrokeWidth()
+                    "font-size": cur_text.font_size,
+                    "font-family": cur_text.font_family,
+                    "text-anchor": "middle",
+                    "xml:space": "preserve",
+                    "opacity": cur_shape.opacity
+                }
+            });
+            newText.textContent = "$NAME$";
+            started = true;
+			return {
+				started: true
+			}
+        },
+        
+        mouseUp: function(opts) {
+            var mode = canv.getMode();
+			if(mode !== mode_id) return;
+            if (! started)
+                return;
+            
+            canv.selectOnly([newText]);
+            canv.textActions.start(newText);
+            return {
+                keep: true,
+                element: newText
+            }
+        }
+	}
+});
+
