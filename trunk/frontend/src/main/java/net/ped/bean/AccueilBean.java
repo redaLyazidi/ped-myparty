5b8c69bab6f567d0ae1684450c0bba9a712df024
sim.moreau79@gmail.com <sim.moreau79@gmail.com@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-01 00:35:53 +0000

    Modif affichage liste des party + modif affichage party sélectionnée

--- trunk/frontend/src/main/java/net/ped/bean/AccueilBean.java
+++ trunk/frontend/src/main/java/net/ped/bean/AccueilBean.java
@@ -38,76 +38,7 @@ public class AccueilBean implements Serializable {
 		listParty = new ArrayList<Party>();
 		numPage = 1;
 		listParty = FrontPartyService.getInstance().getPartiesNotBegunMaxResult(numPage, ConstantesWeb.NUMBER_PARTY_PAGE);
-		
-		/*
-		Adress a1 = new Adress("20 rue du bidon", "Lyon", "6900");
-		Adress a2 = new Adress("12 place bidon", "La Rochelle", "17000");
-		Adress a3 = new Adress("1 avenue bidon", "Bordeaux", "33000");
-		Adress a4 = new Adress("3 rue bidon", "Bordeaux", "33000");
-		Adress a5 = new Adress("45 avenue bidon", "Paris", "75000");
-		Adress a6 = new Adress("52 rue bidon", "Paris", "75000");
-		Adress a7 = new Adress("38 avenue bidon", "Paris", "75000");
-		Adress a8 = new Adress("20 rue bidon", "Niort", "79000");
-		
-		Calendar date = new GregorianCalendar(2013, 04, 15);
-		
-		Party p1 = new Party();
-		p1.setTitle("Super concert");
-		p1.setDateParty(date);
-		p1.setAdress(a1);
-		p1.setPrice(12);
 
-		Party p2 = new Party();
-		p2.setTitle("Guy tar");
-		p2.setDateParty(date);
-		p2.setAdress(a2);
-		p2.setPrice(15);
-		
-		Party p3 = new Party();
-		p3.setTitle("Concert 3 blabla");
-		p3.setDateParty(date);
-		p3.setAdress(a3);
-		p3.setPrice(18);
-		
-		Party p4 = new Party();
-		p4.setTitle("Concert 4 blabla");
-		p4.setDateParty(date);
-		p4.setAdress(a4);
-		p4.setPrice(49);
-		
-		Party p5 = new Party();
-		p5.setTitle("Concert 5 blabla");
-		p5.setDateParty(date);
-		p5.setAdress(a5);
-		p5.setPrice(25);
-		
-		Party p6 = new Party();
-		p6.setTitle("Concert 6 blabla");
-		p6.setDateParty(date);
-		p6.setAdress(a6);
-		p6.setPrice(18);
-		
-		Party p7 = new Party();
-		p7.setTitle("Concert 7 blabla");
-		p7.setDateParty(date);
-		p7.setAdress(a7);
-		p7.setPrice(30);
-		
-		Party p8 = new Party();
-		p8.setTitle("Concert 8 blabla");
-		p8.setDateParty(date);
-		p8.setAdress(a8);
-		p8.setPrice(40);
-		
-		listParty.add(p1);
-		listParty.add(p2);
-		listParty.add(p3);
-		listParty.add(p4);
-		listParty.add(p5);
-		listParty.add(p6);
-		listParty.add(p7);
-		listParty.add(p8);
-		*/
 	}
 	
 	public String outcome() {
--- trunk/frontend/src/main/java/net/ped/bean/PartyBean.java
+++ trunk/frontend/src/main/java/net/ped/bean/PartyBean.java
@@ -1,14 +1,15 @@
 package net.ped.bean;
 
 import java.io.Serializable;
+import java.util.Calendar;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 
 import net.ped.model.Party;
 
 @ManagedBean(name="partyBean")
-@ViewScoped
+@SessionScoped
 public class PartyBean implements Serializable {
 
 	/**
@@ -16,7 +17,19 @@ public class PartyBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	private boolean disableBuyButton;
+	
 	private Party partySelect;
+	private int nbPlaceRest;
+	
+	public PartyBean() {
+		if(partySelect == null ) {
+			System.out.println("yataaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		}
+		else {
+			System.out.println(partySelect.getTitle());
+		}
+	}
 
 	public Party getPartySelect() {
 		return partySelect;
@@ -24,6 +37,35 @@ public class PartyBean implements Serializable {
 
 	public void setPartySelect(Party partySelect) {
 		this.partySelect = partySelect;
+	}
+	
+	public String buyTicket() {
+		return "buyTicket";
+	}
+
+	public boolean isDisableBuyButton() {
+		if(partySelect.getDateBegin().after(Calendar.getInstance().getTime()) &&
+				partySelect.getDateEnd().before(Calendar.getInstance().getTime()) &&
+				partySelect.getNbPlace() > partySelect.getNbPlaceBought()) {
+			disableBuyButton = true;
+		}
+		else {
+			disableBuyButton = false;
+		}
+		return disableBuyButton;
+	}
+
+	public void setDisableBuyButton(boolean disableBuyButton) {
+		this.disableBuyButton = disableBuyButton;
+	}
+
+	public int getNbPlaceRest() {
+			nbPlaceRest = partySelect.getNbPlace() - partySelect.getNbPlaceBought();
+		return nbPlaceRest;
+	}
+
+	public void setNbPlaceRest(int nbPlaceRest) {
+		this.nbPlaceRest = nbPlaceRest;
 	}
 
 }
