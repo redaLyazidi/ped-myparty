7d42d041e8978fe39e5a9626ffbe8dbaf684c626
sim.moreau79@gmail.com <sim.moreau79@gmail.com@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-12 02:59:13 +0000

    Création header + mise en place du login

--- frontend/src/main/java/net/ped/bean/LoginBean.java
+++ frontend/src/main/java/net/ped/bean/LoginBean.java
@@ -3,15 +3,16 @@ package net.ped.bean;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+
 
 import net.ped.constante.ConstantesWeb;
 import net.ped.model.User;
-
+import net.ped.service.front.FrontPartyService;
 
 @ManagedBean(name="loginBean")
-@ViewScoped
+@SessionScoped
 public class LoginBean implements Serializable {
 
 	/**
@@ -21,19 +22,42 @@ public class LoginBean implements Serializable {
 	
 	private String login;
 	private String password;
-	private boolean loginSuccess = false;
+	private boolean loginSuccess;
 	private String errorMsg;
-	private boolean connectFailed = false;
+	private boolean connectFailed;
 	private User userLogged;
+	private boolean userAdmin;
+	
+	public LoginBean() {
+		this.errorMsg = ConstantesWeb.ERROR_MESSAGE_LOGIN;
+	
+		this.loginSuccess = false;
+		this.connectFailed = false;
+		this.userAdmin = false;
+		
+	}
 	
 	public String connect() {
-		if(login.equals("login") && password.equals("login")) {
-			this.loginSuccess = true;
-			this.connectFailed = false;
-		}
-		else {
-			this.loginSuccess = false;
-			this.connectFailed = true;
+		
+		if(login != null && password != null ) {
+			
+			userLogged = FrontPartyService.getInstance().login(login, password);
+			//userLogged = FrontPartyService.getInstance().login("jean", "123");
+			
+			if(userLogged != null) {
+			
+				this.loginSuccess = true;
+				this.connectFailed = false;
+				
+				if(userLogged.getRole().equals("admin")) {
+					this.userAdmin = true;
+				}
+			}
+			else {
+				this.loginSuccess = false;
+				this.connectFailed = true;
+				
+			}
 		}
 		
 		return "login";
@@ -43,6 +67,7 @@ public class LoginBean implements Serializable {
 		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
 		userLogged = null;
 		this.loginSuccess = false;
+		this.userAdmin = false;
 		
 		return "logout";
 	}
@@ -68,42 +93,41 @@ public class LoginBean implements Serializable {
 		return loginSuccess;
 	}
 
-
 	public void setLoginSuccess(boolean loginSuccess) {
 		this.loginSuccess = loginSuccess;
 	}
 
-
 	public String getErrorMsg() {
-		if(!loginSuccess && connectFailed) {
-			errorMsg = ConstantesWeb.ERROR_MESSAGE_LOGIN;
-		}
+
 		return errorMsg;
 	}
-
 
 	public void setErrorMsg(String errorMsg) {
 		this.errorMsg = errorMsg;
 	}
 
-
 	public boolean isConnectFailed() {
 		return connectFailed;
 	}
-
 
 	public void setConnectFailed(boolean connectFailed) {
 		this.connectFailed = connectFailed;
 	}
 
-
 	public User getUserLogged() {
 		return userLogged;
 	}
 
-
 	public void setUserLogged(User userLogged) {
 		this.userLogged = userLogged;
+	}
+
+	public boolean isUserAdmin() {
+		return userAdmin;
+	}
+
+	public void setUserAdmin(boolean userAdmin) {
+		this.userAdmin = userAdmin;
 	}
 	
 
