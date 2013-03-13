90eabe056c3a95205c510ac48a78a216d7b0a160
reda.lyazidiProjet <reda.lyazidiProjet@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-13 10:05:29 +0000

    Correction due to the method getInstance of MyHttpServlet becoming static in Commons.java

--- frontend/src/main/java/net/ped/shared/Commons.java
+++ frontend/src/main/java/net/ped/shared/Commons.java
@@ -3,7 +3,6 @@ package net.ped.shared;
 import java.io.File;
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,36 +10,49 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Commons {
-	
-	private static final String ticketsPath = "resources/tickets/";
+
 	private static final String ticketsextension =".svg";
-	
+
 	private static final Logger LOG = LoggerFactory.getLogger(Commons.class);
-	private static MyHttpServlet myservlet = new MyHttpServlet();
-	
+	private static HttpServlet myservlet = MyHttpServlet.getInstance();
+
 	public static String getMandatoryStringParameter(HttpServletRequest request, String name) throws Exception {
 		String param = request.getParameter(name);
 		if (param == null)
 			throw new Exception();
 		return param;
 	}
-	
+
 	public static int getMandatoryIntParameter(HttpServletRequest request, String name) throws Exception {
 		return Integer.getInteger(getMandatoryStringParameter(request, name));
 	}
-	
+
 	public static File getPartySvgFile(int idParty) throws IOException {
-		String webappPath = myservlet.getInstance().getServletContext().getRealPath("/");
-		String fromparameter = myservlet.getInstance().getServletContext().getInitParameter("ticketsDir");
+		
+		if (idParty < 0) {
+			LOG.debug("The id can't be lower than 0, id = " + idParty);
+			return null;
+		}
+		
+		
+		
+		String webappPath = myservlet.getServletContext().getRealPath("/");
+		String fromparameter = myservlet.getServletContext().getInitParameter("ticketsDir");
 		LOG.info("fromparameter: " + fromparameter);
 		String absoluteticketsPath =new StringBuilder(webappPath).append(fromparameter).append("ticket").append(ticketsextension).toString();
 		LOG.info("absoluteticketsPath: " + absoluteticketsPath);
 		File ticketsDir = new File(absoluteticketsPath);
 		String thisPartyticketPath = new StringBuilder(absoluteticketsPath).append(idParty).append(ticketsextension).toString();
+		
+		File ticketOfIdParty = new File(thisPartyticketPath);
+		if (! ticketOfIdParty.exists()) {
+			LOG.debug("There no svg file withe name: " + idParty);
+			return null;
+		}
 		LOG.info(thisPartyticketPath);
-		LOG.info( myservlet.getInstance().getServletContext().getInitParameter("svgQRcodetag"));
-		
-		
+		LOG.info( myservlet.getServletContext().getInitParameter("svgQRcodetag"));
+
+
 		LOG.info("before the exec");
 		Runtime runtime = Runtime.getRuntime();
 		LOG.info(webappPath + fromparameter);
@@ -52,4 +64,5 @@ public class Commons {
 			LOG.info("I found it");
 		return null;
 	}
+	
 }
