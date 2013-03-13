c6ebd37344f60113978349a1ee4bb5ebf84aee78
flguerin3@gmail.com <flguerin3@gmail.com@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-13 17:37:00 +0000

    Ajout fonctionnalité Suppression Party pour l'admin si préventes non commencées

--- frontend/src/main/java/net/ped/bean/PartyBean.java
+++ frontend/src/main/java/net/ped/bean/PartyBean.java
@@ -2,12 +2,15 @@ package net.ped.bean;
 
 import java.io.Serializable;
 import java.util.Calendar;
+import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
+import net.ped.constante.ConstantesWeb;
 import net.ped.model.Party;
+import net.ped.service.front.FrontPartyService;
 
 @ManagedBean(name="partyBean")
 @SessionScoped
@@ -16,9 +19,14 @@ public class PartyBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private boolean disableBuyButton;
+	private Calendar dateCourante;
 	
 	@ManagedProperty(value="#{accueilBean}")
 	private AccueilBean accueilBean;
+	
+	public PartyBean(){
+		dateCourante = Calendar.getInstance();
+	}
 	
 	public String buyTicket() {
 		return "buyTicket";
@@ -46,5 +54,19 @@ public class PartyBean implements Serializable {
 
 	public void setAccueilBean(AccueilBean accueilBean) {
 		this.accueilBean = accueilBean;
+	}
+	
+	public Calendar getDateCourante() {
+		return dateCourante;
+	}
+
+	public void setDateCourante(Calendar dateCourante) {
+		this.dateCourante = dateCourante;
+	}
+
+	public String deleteParty(){
+		FrontPartyService.getInstance().deleteParty(accueilBean.getIdParty());
+		accueilBean.setListParty(FrontPartyService.getInstance().getPartiesNotBegunMaxResult(0, ConstantesWeb.NUMBER_PARTY_PAGE));
+		return "accueil";
 	}
 }
