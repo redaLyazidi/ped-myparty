d7a9567429e9be1dd051dd528d20a4379da19c89
nicolas.moreaud@gmail.com <nicolas.moreaud@gmail.com@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-14 10:10:16 +0000

    Refactores ticket rasterizer. Problem in the ticketCustomerRasterizer : needs the svg string

---
+++ frontend/src/main/java/net/ped/servlet/TicketCustomerRasterizer.java
@@ -0,0 +1,49 @@
+package net.ped.servlet;
+
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.ped.dao.BillingDaoImpl;
+import net.ped.model.Ticket;
+import net.ped.shared.Commons;
+
+
+@SuppressWarnings("serial")
+public class TicketCustomerRasterizer extends TicketRasterizer {
+	protected TicketInformation getTicketInformations(HttpServletRequest request, HttpServletResponse response) {
+		TicketInformation ticketInfos = new TicketInformation();
+
+		try {
+			ticketInfos.idParty = Commons.getMandatoryIntParameter(request,	"idparty");
+			ticketInfos.idClient = Commons.getMandatoryIntParameter(request,"idclient");
+			ticketInfos.secretCode = Commons.getMandatoryStringParameter(request, "secretcode");
+		} catch (Exception e) {
+			return null;
+		}
+		return ticketInfos;
+	}
+
+	protected boolean checkValidTicketInformation(TicketInformation ticketInfos) {
+		return (ticketInfos.idClient >= 0 && ticketInfos.idParty >= 0 && ticketInfos.secretCode
+				.length() == 16);
+	}
+
+	protected Ticket checkAuthorizedAccess(TicketInformation ticketInfos) {
+		Ticket ticket;
+		try {
+			ticket = new BillingDaoImpl().getTicket(ticketInfos.idParty, ticketInfos.idClient); // TODO : check param order !!
+		} catch(Exception e) {
+			return null;
+		}
+		if (ticketInfos.secretCode != ticket.getSecretCode())
+			return null;
+		return ticket;
+	}
+
+	@Override
+	protected File getGenericSvgTicket(TicketInformation infos) {
+		return Commons.getPartySvgFile(infos.idParty);
+	}
+}
