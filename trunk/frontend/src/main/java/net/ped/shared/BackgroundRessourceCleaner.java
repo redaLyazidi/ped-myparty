530e33b0a399e0cc9fd8297e6c541e69c5aca7e0
nicolas.moreaud@gmail.com <nicolas.moreaud@gmail.com@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-14 13:17:53 +0000

    Added batch old file cleaner

---
+++ trunk/frontend/src/main/java/net/ped/shared/BackgroundRessourceCleaner.java
@@ -0,0 +1,98 @@
+package net.ped.shared;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackgroundRessourceCleaner implements Runnable {
+	protected static final Logger LOG = LoggerFactory	.getLogger(BackgroundRessourceCleaner.class);
+	private static BackgroundRessourceCleaner instance = null;
+	private List<Pair<File, Pattern>> locations = new ArrayList<Pair<File, Pattern>>();
+	private int checkIntervalSeconds;
+	
+	private BackgroundRessourceCleaner(int checkIntervalSeconds) {
+		this.checkIntervalSeconds = checkIntervalSeconds;
+	}
+
+	public static BackgroundRessourceCleaner getInstance(int checkIntervalSeconds) {
+		if (instance == null)
+			instance = new BackgroundRessourceCleaner(checkIntervalSeconds);
+		return instance;
+	}
+
+	public void watchFiles(File directory, Pattern filenamePattern)
+			throws Exception {
+		if (!directory.isDirectory())
+			throw new Exception();
+		LOG.info("Added an entry to watch and auto delete files : "
+				+ directory.getAbsolutePath());
+		synchronized (locations) {
+			locations.add(Pair.of(directory, filenamePattern));
+		}
+	}
+
+	@Override
+	public void run() {
+		LOG.info("Cleaning the resources...");
+		List<Pair<File, Pattern>> locationsCopy = null;
+		synchronized (locations) {
+			locationsCopy = new ArrayList<Pair<File, Pattern>>(locations);
+		}
+
+		for (Pair<File, Pattern> p : locationsCopy) {
+			File dir = p.getLeft();
+			Pattern filenamePattern = p.getRight();
+			try {
+				File[] contents = dir.listFiles();
+				if (contents == null) {
+					LOG.warn("Directory : " + dir + " isn't readable");
+					continue;
+				}
+				for (File f : contents) {
+					deleteIfTooOld(f, filenamePattern);
+				}
+			} catch (Exception e) {
+				LOG.warn("Directory : " + dir + " isn't readable");
+			}
+		}
+		LOG.info("Cleaning the resources... done !");
+	}
+
+	private void deleteIfTooOld(File f, Pattern filenamePattern) {
+		if (f.isDirectory() == true)
+			return;
+
+		Matcher m = filenamePattern.matcher(f.getName());
+		if (m.find() == true) {
+			LOG.debug("Checking : " + f.getName());
+			if (isTooOld(f.lastModified()) == false) {
+				LOG.debug(f.getName() + " is recent");
+				return;
+			}
+
+			LOG.info("Delete old file : " + f.getName());
+			try {
+				f.delete();
+			} catch (Exception e) {
+				LOG.info("Can't delete old file : " + f.getName());
+			}
+		}
+	}
+
+	private boolean isTooOld(long lastModif) {
+		DateTime now = new DateTime();
+		DateTime lastModif2 = new DateTime(new Date(lastModif));
+		Duration ellapsed = new Duration(lastModif2, now);
+		LOG.debug("time ellapsed : " + ellapsed.getStandardSeconds());
+		return (ellapsed.getStandardSeconds() >= this.checkIntervalSeconds);
+	}
+}
---
+++ trunk/frontend/src/main/java/net/ped/shared/BackgroundRessourceCleanerScheduler.java
@@ -0,0 +1,25 @@
+package net.ped.shared;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class BackgroundRessourceCleanerScheduler implements ServletContextListener {
+	private ScheduledExecutorService scheduler;
+	private static final int checkIntervalSeconds = 10; 
+
+	@Override
+	public void contextInitialized(ServletContextEvent event) {
+		scheduler = Executors.newSingleThreadScheduledExecutor();
+		BackgroundRessourceCleaner task = BackgroundRessourceCleaner.getInstance(checkIntervalSeconds);
+		scheduler.scheduleAtFixedRate(task, 0, checkIntervalSeconds, TimeUnit.SECONDS);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent event) {
+		scheduler.shutdownNow();
+	}
+}
