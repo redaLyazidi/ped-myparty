a767fccaa9f02a5ce6438c81008453fc49673ffc
reda.lyazidiProjet <reda.lyazidiProjet@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-22 16:25:36 +0000

    Conversion SVG to PDF works with inkscape

--- frontend/src/main/java/net/ped/shared/InkscapeSvgToPdf.java
+++ frontend/src/main/java/net/ped/shared/InkscapeSvgToPdf.java
@@ -1,5 +1,6 @@
 package net.ped.shared;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -9,26 +10,41 @@ import org.slf4j.LoggerFactory;
 public class InkscapeSvgToPdf implements SvgToPdf {
 	
 	protected static final Logger LOG = LoggerFactory.getLogger(SvgToPdf.class);
-
+	protected static final int bufferSize = 4096;
+	
 	@Override
 	public boolean convert(File svg, File output) {
 		//String svgfullname = svg.getName();
 		//String svgname = svgfullname.substring(0, svgfullname.length() -4);
 		StringBuilder cmdBuilder = new StringBuilder();
-		cmdBuilder.append("inscape -f ").append(svg.getAbsolutePath()).append(" -A ").append(output.getAbsolutePath());
+		cmdBuilder.append("inkscape -f ").append(svg.getAbsolutePath()).append(" -A ").append(output.getAbsolutePath());
 		//String cmd = "inkscape -f " + svg.getAbsolutePath() + " -A " + svg.getParent() + '/' + svg.getName() + ".pdf";
 		String cmd = cmdBuilder.toString();
 		LOG.info("cmd: " + cmd);
 		try {
 			Runtime runtime = Runtime.getRuntime();
-			runtime.exec(cmd);
-			int delay = 3 * 1000;
+			
+			Process process = runtime.exec(cmd);
+			BufferedInputStream bufin = new BufferedInputStream(process.getInputStream());
+			byte[] buffer = new byte[bufferSize];
+			
+			while (bufin.read(buffer) != -1)
+				;
+			
+			try {
+				process.waitFor();
+			} catch (InterruptedException e) {
+				LOG.debug("inkscape was interrupted");
+				e.printStackTrace();
+				return false;
+			}
+			/*int delay = 3 * 1000;
 			LOG.debug("Let me sleep some time...");
 	        try {
 	            Thread.sleep(delay);
 	        } catch (InterruptedException e1) {
 	        	LOG.debug("I can't sleep...");
-	        }
+	        }*/
 	        LOG.debug("Ahhh... let's go !");
 			
 		} catch (SecurityException se) {
