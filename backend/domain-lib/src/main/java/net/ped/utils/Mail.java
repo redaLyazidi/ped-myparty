c87a327af558996d05484cbd295511537abf204c
nicolas.moreaud@gmail.com <nicolas.moreaud@gmail.com@1c0b7e0a-9e1f-872f-89a0-47e022137633>
2013-03-27 17:11:15 +0000

    added tickets and fixed mail settings

--- backend/domain-lib/src/main/java/net/ped/utils/Mail.java
+++ backend/domain-lib/src/main/java/net/ped/utils/Mail.java
@@ -34,16 +34,16 @@ public class Mail {
 	
 	public void sendMail(Customer customer, Party party, String urlTicket) throws MessagingException {
 
-		Properties prop;
+
 		try {
-			
+//			Properties prop;
 //			prop = PropertyLoader.load("../java/net/utils/mail.properties");
 //			final String mail = prop.getProperty("mailMyParty");
 //			final String password = prop.getProperty("passwordMyParty");
 //			final String object = prop.getProperty("object");
 		
-			final String mail = "myparty@hotmail.fr";
-			final String password = "PASSword123";
+			final String mail = "projet.img3d@gmail.com";
+			final String password = "blopblop";
 			final String object = "Confirmation de réservation";
 			final String content = "<html><body> " +
 					"<h2>Confirmation de réservation</h2>" +
@@ -51,16 +51,16 @@ public class Mail {
 					"Votre réservation pour " + party.getTitle() + ", le " +
 					party.getDateParty().getTime() + ", d'un montant de " +
 					party.getPrice() + ", a bien été pris en compte. <br/>" +
-					"Veuillez présenter le ticket à l'entré du concert, disponible " +
-					"<a href=\" "+ urlTicket +" \">ici.</a>" +
+					"Veuillez présenter le ticket à l'entré du concert, " +
+					"<a href=\" "+ urlTicket +" \">disponible ici</a>." +
 					"<br/><br/>" +
 					"MyParty.fr </body></html>";
 					
 			Properties props = new Properties();
-			props.put("mail.smtp.host", "smtp.live.com");
+			props.put("mail.smtp.auth", "true");
 			props.put("mail.smtp.starttls.enable", "true");
-		    props.put("mail.smtp.auth", "true");
-		    props.put("mail.smtp.port", "587");
+			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.port", "587");
 
 		    Session session = Session.getInstance(props,
 	            new javax.mail.Authenticator() {
@@ -71,8 +71,7 @@ public class Mail {
 
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(mail));
-            message.setRecipients(Message.RecipientType.TO, 
-                InternetAddress.parse(customer.getMail()));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(customer.getMail()));
             message.setSubject(object);
             message.setContent(content, "text/html");
 
